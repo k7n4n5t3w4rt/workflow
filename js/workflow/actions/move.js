e135eb3be703ae63038c807a65c16a1297dbd252
Kynan Stewart Hughes <mail@kynan.net.au>
2023-06-02 19:44:21 +1000

    ...

--- js/workflow/actions/move.js
+++ js/workflow/actions/move.js
@@ -6,29 +6,18 @@ import anime from "../../../web_modules/animejs.js";
 import gSettings from "./gSettings.js";
 
 const move = (workFlowItem /*: Object */) /*: void */ => {
-  // NOTE:
-  // This might not be very clear so:
-  //
-  // cubes is an an array of columns of cubes.
-  // Each cube object is a REFERENCE to a THREE.js Mesh object that
-  // was atached to the THREE.js scene in:
-  //
-  //		/js/workflow/actions/pixelGrid.js (Line 34)
-  //
-  const speed = gSettings().speed;
-
   // Move cube1
   anime({
     targets: [workFlowItem.position],
     z: workFlowItem.position.z + gSettings().zCm * 2,
     // Every team has its own row of cubes.
     y: workFlowItem.teamNumber * gSettings().yCm * 2,
-    duration: 1000 / speed,
+    duration: 1000 / gSettings().speed,
     delay: 0,
     easing: "easeInOutCirc",
     complete: function (anim) {
-      console.log("Move complete.");
-      // Move the next cube...
+      // console.log("Move complete.");
+      // workFlowItem.status = gSettings().workflowStatuses[1];
     },
   });
 };
