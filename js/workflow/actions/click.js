9f1084fbbfed5bbe3b553425c94ba671ed28e5b8
Kynan Stewart Hughes <mail@kynan.net.au>
2023-06-09 08:22:40 +1000

    ...

--- js/workflow/actions/click.js
+++ js/workflow/actions/click.js
@@ -63,10 +63,6 @@ function pullFlwItems() {
       }
     },
   );
-
-  // flwMpSteps.map((flwMpItmKey /*: string */) /*: FlwMpItems */ => {
-  //   return gState().flwMap[flwMpStpKey];
-  // })
 }
 
 function getFlwMpSteps() /*: FlwMpItems[] */ {
